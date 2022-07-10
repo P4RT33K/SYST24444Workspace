0996d26a10477c0a22048c1ef38386f98b37508b
P4RT33K <parteekpar00@gmail.com>
2022-07-10 13:23:18 -0400

    Added Assign02 & Lab08!

---
+++ Assignment2-parteek/js/script.js
@@ -0,0 +1,10 @@
+// Name - Parteek
+// Email - parteek@sheridancollege.ca
+// Date - 7 July, 2022
+// Description - This file has all the javascript functions for the assignment
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main(){
+
+}
