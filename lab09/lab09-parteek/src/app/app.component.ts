ddba7cec1ac7167581fea5c3709098df3c691f18
P4RT33K <parteekpar00@gmail.com>
2022-07-14 16:20:29 -0400

    Completed Lab09!

--- lab09/lab09-parteek/src/app/app.component.ts
+++ lab09/lab09-parteek/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// constant: conversion factor from lb to kg
+const FACT = 0.45359237;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab09-parteek';
+
+  lb = 1; // public property
+  kg = Math.round(this.lb * FACT * 100) / 100; // public property
+
+  convertToKg(){
+    this.kg = Math.round(this.lb * FACT * 100) / 100;
+  }
+  convertToLb(){
+    this.lb = Math.round(this.kg / FACT * 100) / 100;
+  }
 }
